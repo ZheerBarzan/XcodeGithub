8965dd96449243378db714b6b95f7cdc2c708121
zheer barzan <zheerbarzan85@outlook.com>
2024-03-15 01:35:02 +0300

    Initial Commit

---
+++ XcodeGithub/XcodeGithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeGithubApp.swift
+//  XcodeGithub
+//
+//  Created by zheer barzan on 3/15/24.
+//
+
+import SwiftUI
+
+@main
+struct XcodeGithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
